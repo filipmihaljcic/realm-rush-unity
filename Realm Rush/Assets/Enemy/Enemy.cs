b62a07a8a860aa607503b9f940a63b23a91e67af
Filip Mihaljčić <filip.mihaljcic@yahoo.com>
2022-04-30 18:31:27 +0200

    Add Project

---
+++ Realm Rush/Assets/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] int goldReward = 25;
+        [SerializeField] int goldPenalty = 25;
+
+        Bank bank;
+
+        private void Start()
+        {
+            bank = FindObjectOfType<Bank>();        
+        }
+
+        public void RewardGold()
+        {
+            if(bank == null) { return; }
+            bank.Deposit(goldReward);
+        }
+
+        public void StealGold()
+        {
+            if(bank == null) { return; }
+            bank.Withdraw(goldPenalty);
+        }
+    }
+}
---
+++ Realm Rush/Assets/Enemy/ObjectPool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Project.Scripts
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField] GameObject enemyPrefab;
+        [SerializeField] [Range(0, 50)] int poolSize = 5;
+        [SerializeField] [Range(0.1f, 30f)] float spawnTimer = 1f;
+    
+        GameObject[] pool;
+
+        private void Awake()
+        {
+            PopulatePool();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(SpawnEnemy());
+        }
+
+        private void PopulatePool()
+        {        
+            pool = new GameObject[poolSize];
+
+            for(int i = 0; i < pool.Length; i++)
+            {
+                pool[i] = Instantiate(enemyPrefab, transform);
+                pool[i].SetActive(false);
+            }
+        }
+
+        private void EnableObjectInPool()
+        {
+            for(int i = 0; i < pool.Length; i++)
+            {
+                if(pool[i].activeInHierarchy == false)
+                {
+                    pool[i].SetActive(true);
+                    return;
+                }
+            }
+        }
+    
+        IEnumerator SpawnEnemy()
+        {
+            while(true)
+            {
+                EnableObjectInPool();
+                yield return new WaitForSeconds(spawnTimer);
+            }
+        }
+    }
+}
